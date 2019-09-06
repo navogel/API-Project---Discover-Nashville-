8a80cd97b1d1af29ba7fe59beb4f4879916f8268
Nate V <navogel@gmail.com>
2019-09-06 15:12:10 -0500

    not sure what is going on

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,12 @@ const meetupArray = [];
 const concertArray = [];
 const parkArray = [];
 const restaurantArray = [];
-// const itinObj = {
-// 	park: {},
-// 	restaurant: {},
-// 	meetup: {},
-// 	concert: {}
-// };
+const itinObj = {
+	park: {},
+	restaurant: {},
+	meetup: {},
+	concert: {}
+};
 
 // web Component station
 
@@ -253,68 +253,37 @@ document.querySelector("#search-parks").addEventListener("click", event => {
 // 	).innerHTML = WEB.createItinerary();
 // };
 
-allArrays = {
-	meetup: ["yes", "no"],
-	concert: [],
-	park: [],
-	restaurant: []
-};
-
-const itinObj = {
-	park: {},
-	concert: {},
-	meetup: {},
-	restaurant: {}
-};
-
 document
 	.querySelector("#search-results-container")
-	.addEventListener("click", taco => {
-		// console.log(taco.target.name);
-		// console.log(taco.target.id);
-		var targetID = taco.target.id;
-		// selectedArray = allArrays[taco.target.name]
-		selectedElement = allArrays[taco.target.name][targetID];
-		itinObj[taco.target.name] = selectedElement;
-		console.log(initObj);
-		// console.log(taco);
-
-		// let itemID = event.target.id;
-		// let arrayID = event.target.name;
-		// let arrayObj = someArray[itemID];
+	.addEventListener("click", event => {
+		let i = event.target.id;
+		if (event.target.name.startsWith("concert")) {
+			itinObj.concert = concertArray[i];
+			console.log(itinObj.concert);
+			document.querySelector(
+				"#itinerary-container"
+			).innerHTML = WEB.createConcertItinerary(itinObj.concert);
+			console.log(itinObj);
+		} else if (event.target.name.startsWith("restaurant")) {
+			itinObj.restaurant = restaurantArray[i].restaurant;
+			document.querySelector(
+				"#itinerary-container"
+			).innerHTML = WEB.createRestaurantItinerary(itinObj.restaurant);
+			console.log(itinObj);
+		} else if (event.target.name.startsWith("park")) {
+			itinObj.park = parkArray[i];
+			document.querySelector(
+				"#itinerary-container"
+			).innerHTML = WEB.createParkItinerary(itinObj.park);
+			console.log(itinObj);
+		} else if (event.target.name.startsWith("meetup")) {
+			itinObj.meetup = meetupArray[i];
+			document.querySelector(
+				"#itinerary-container"
+			).innerHTML = WEB.createMeetupItinerary(itinObj.meetup);
+			console.log(itinObj);
+		}
 	});
-
-// document
-// 	.querySelector("#search-results-container")
-// 	.addEventListener("click", event => {
-// 		let i = event.target.id;
-// 		if (event.target.name.startsWith("concert")) {
-// 			itinObj.concert = concertArray[i];
-// 			console.log(itinObj.concert);
-// 			document.querySelector(
-// 				"#itinerary-container"
-// 			).innerHTML = WEB.createConcertItinerary(itinObj.concert);
-// 			console.log(itinObj);
-// 		} else if (event.target.name.startsWith("restaurant")) {
-// 			itinObj.restaurant = restaurantArray[i].restaurant;
-// 			document.querySelector(
-// 				"#itinerary-container"
-// 			).innerHTML = WEB.createRestaurantItinerary(itinObj.restaurant);
-// 			console.log(itinObj);
-// 		} else if (event.target.name.startsWith("park")) {
-// 			itinObj.park = parkArray[i];
-// 			document.querySelector(
-// 				"#itinerary-container"
-// 			).innerHTML = WEB.createRestaurantItinerary(itinObj.park);
-// 			console.log(itinObj);
-// 		} else if (event.target.name.startsWith("meetup")) {
-// 			itinObj.meetup = meetupArray[i];
-// 			document.querySelector(
-// 				"#itinerary-container"
-// 			).innerHTML = WEB.createRestaurantItinerary(itinObj.meetup);
-// 			console.log(itinObj);
-// 		}
-// 	});
 
 //   function savePark(clickedPark) {
 // 		let selectedPark = clickedPark.value
